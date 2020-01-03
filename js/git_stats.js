7460006b4970c75b764adc14e275897ff4da5630
ishant <ishantdahiya@gmail.com>
2020-01-04 02:33:40 +0530

    Minor changes in git stats.js

--- js/git_stats.js
+++ js/git_stats.js
@@ -4,9 +4,9 @@ $.getJSON("../scripts/data/result.json", function(data) {
   let languages = data[2];
   const repos = data[3];
   var l = document.getElementById("loc");
-  l.setAttribute("data-to", lines);
+  l.setAttribute("data-to", lines - 0.35 * lines);
   var f = document.getElementById("files");
-  f.setAttribute("data-to", files);
+  f.setAttribute("data-to", files - 0.35 * files);
   var lang = document.getElementById("languages");
   lang.setAttribute("data-to", languages - 10);
   var repo = document.getElementById("repos");
